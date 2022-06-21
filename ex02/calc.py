70973dbd8f4dd52403544db6cffcae2ac9f764d9
nakano <c0a21094f2@edu.teu.ac.jp>
2022-06-21 15:25:54 +0900

    練習問題7

--- ex02/calc.py
+++ ex02/calc.py
@@ -1,11 +1,19 @@
 import tkinter as tk
 import tkinter.messagebox as tkm
 
+
 def button_click(event):
     button = event.widget
     num = button["text"]
     #tkm.showinfo("",f"{num}のボタンが押されました")
     entry.insert(tk.END, num)
+
+def eqall_click(event):
+    a = entry.get()
+    result = eval(a)
+    entry.delete(0,tk.END)
+    entry.insert(tk.END, result)
+
 
 if __name__ == "__main__":
     root = tk.Tk()
@@ -16,11 +24,13 @@ if __name__ == "__main__":
     entry.grid(row=0, column=0, columnspan=3)
 
     r,c=1,0
-    for i, num in enumerate([9, 8, 7, 6, 5, 4, 3, 2, 1, 0]+["+"]):
+    for i, num in enumerate([9, 8, 7, 6, 5, 4, 3, 2, 1, 0]+["+", "="]):
         button =tk.Button(root,text=num,width=4, height=2,font=("Times New Roman",30))
         button.bind("<1>", button_click)
-        button.grid(row=r,column=c)
+        button.grid(row=r, column=c)
         c+=1
+        if num == "=":
+            button.bind("<1>", eqall_click)
         if (i+1)%3==0:
             r+=1
             c=0
