ea61ef56790195f41ddd004b3292ee6df3f8ff03
nakano <c0a21094f2@edu.teu.ac.jp>
2022-06-21 14:05:08 +0900

    練習問題１

--- ex02/calc.py
+++ ex02/calc.py
@@ -3,10 +3,5 @@ import tkinter.messagebox as tkm
 
 root = tk.Tk()
 root.geometry("300x500")
-root.geometry("300x500")
-font1=("Times New Roman", 30)
-for i in range(9, -1, -1):
-    button = tk.Button(root, text=i, font=font1)
-    button.grid(row=0, column=0)
-    button.pack()
+
 root.mainloop()
