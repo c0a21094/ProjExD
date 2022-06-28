c5e152c06a45a747346a3e928b49f33b8aace712
nakano <c0a21094f2@edu.teu.ac.jp>
2022-06-28 13:53:34 +0900

    3回目

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,9 +1,17 @@
 import tkinter as tk
-import tkinter.messagebox as tkm
 
-root = tk.Tk()
-root.title("迷えるこうかとん")
-root.geometry("1500x900")
-root.configure(bg='black')
 
-root.mainloop()
+
+if __name__== "__main__":
+    root = tk.Tk()
+    root.title("迷えるこうかとん")
+    root.geometry()
+ 
+    cx = 300 # こうかとんのx座標
+    cy = 400 # こうかとんのy座標
+    tori = tk.PhotoImage(file="fig/0.png", width=cx, height=cy)
+    canvas = tk.Canvas(root, bg="#000000", height=900, width=1500)
+    canvas.create_image(cx, cy, image=tori, tag="tori")
+    canvas.pack()
+
+    root.mainloop()
