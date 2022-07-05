c78b192c7db61e346e19839c644f8542bb0695df
nakano <c0a21094f2@edu.teu.ac.jp>
2022-07-05 14:01:29 +0900

    問3

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -6,13 +6,16 @@ def main():
     screen = pg.display.set_mode((1600, 900))
     bg = pg.image.load("pg_bg.jpg").convert_alpha() # surfaceクラス
     rect_bg = bg.get_rect()
-
+    tori_img = pg.image.load("fig/6.png")
+    tori_img = pg.transform.rotozoom(tori_img, 0, 2.0)
+    tori_rect = tori_img.get_rect()
+    tori_rect.center = 900, 400
     while (1):
         pg.display.update()
         pg.time.wait(30)
         screen.fill((0, 20, 0, 0))
         screen.blit(bg, rect_bg)
-
+        screen.blit(tori_img, tori_rect)
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 pg.quit()
