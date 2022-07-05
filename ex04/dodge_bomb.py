45338f4994e82d1511e7dd1fff7bfdb1253f95ab
nakano <c0a21094f2@edu.teu.ac.jp>
2022-07-05 14:22:24 +0900

    問4

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -10,12 +10,30 @@ def main():
     tori_img = pg.transform.rotozoom(tori_img, 0, 2.0)
     tori_rect = tori_img.get_rect()
     tori_rect.center = 900, 400
+    key_lst = pg.key.get_pressed()
+#    if key_lst[pg.K_UP] == True:
+
+
     while (1):
         pg.display.update()
         pg.time.wait(30)
         screen.fill((0, 20, 0, 0))
         screen.blit(bg, rect_bg)
         screen.blit(tori_img, tori_rect)
+
+        key_lst = pg.key.get_pressed()
+        if key_lst[pg.K_UP]:
+            tori_rect.move_ip(0, -1)
+
+        if key_lst[pg.K_DOWN]:
+            tori_rect.move_ip(0, 1)
+
+        if key_lst[pg.K_RIGHT]:
+            tori_rect.move_ip(1, 0)
+            
+        if key_lst[pg.K_LEFT]:
+            tori_rect.move_ip(-1, 0)
+        
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 pg.quit()
@@ -25,6 +43,8 @@ def main():
                 if event.key == pg.K_ESCAPE:
                     pg.quit()
                     sys.exit()
+
+        
 
 if __name__ == "__main__":
     pg.init()
