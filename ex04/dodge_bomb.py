e60c0e087128cf27083b4c577371ffffb12a82c4
nakano <c0a21094f2@edu.teu.ac.jp>
2022-07-05 13:59:16 +0900

    問2

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -5,8 +5,23 @@ def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((1600, 900))
     bg = pg.image.load("pg_bg.jpg").convert_alpha() # surfaceクラス
-    bg = pg.transform.rotozoom(bg, 0, 1) # surface
+    rect_bg = bg.get_rect()
 
+    while (1):
+        pg.display.update()
+        pg.time.wait(30)
+        screen.fill((0, 20, 0, 0))
+        screen.blit(bg, rect_bg)
+
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                pg.quit()
+                sys.exit()
+
+            if event.type == pg.KEYDOWN:
+                if event.key == pg.K_ESCAPE:
+                    pg.quit()
+                    sys.exit()
 
 if __name__ == "__main__":
     pg.init()
